42e4fff491d9cbc47377fc5e09e335b6fc0827b2
Alina Stewart <alinastewart@Alinas-iMac.local>
2021-01-26 15:12:09 -0800

    This program has a method that takes a String called fname as parameter. When the method is called, we pass along a first name, which is used inside the method to print the full name.

---
+++ myFullName.java
@@ -0,0 +1,12 @@
+
+public class myFullName {
+  static void myMethod(String fname) {
+    System.out.println(fname + " Stewart");
+  }
+
+  public static void main(String[] args) {
+    myMethod("Liam");
+    myMethod("Jenny");
+    myMethod("Anja");
+  }
+}
